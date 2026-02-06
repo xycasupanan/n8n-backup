6fb90a0e8ce68045aa46d255238fc1f97190d98a
xycasupanan <xycasupanan@users.noreply.github.com>
2026-02-06 02:45:02 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict YffVj3hQEOXHOhWPIKdPcSZy36CtsrZbUR8l1QXGLxuSw4lidL6ulYD5Cbcfpcb
+-- \restrict kDbqjkDGskJ9Safm2mzYiOA98fRzQNI4qQq60WfRLRJBe8QJapor1raRJCgDEIP
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -33,7 +33,7 @@ COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "
 -- Data for Name: flow_state; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."flow_state" ("id", "user_id", "auth_code", "code_challenge_method", "code_challenge", "provider_type", "provider_access_token", "provider_refresh_token", "created_at", "updated_at", "authentication_method", "auth_code_issued_at") FROM stdin;
+COPY "auth"."flow_state" ("id", "user_id", "auth_code", "code_challenge_method", "code_challenge", "provider_type", "provider_access_token", "provider_refresh_token", "created_at", "updated_at", "authentication_method", "auth_code_issued_at", "invite_token", "referrer", "oauth_client_state_id", "linking_target_id", "email_optional") FROM stdin;
 \.
 
 
@@ -65,7 +65,7 @@ COPY "auth"."instances" ("id", "uuid", "raw_base_config", "created_at", "updated
 -- Data for Name: oauth_clients; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."oauth_clients" ("id", "client_secret_hash", "registration_type", "redirect_uris", "grant_types", "client_name", "client_uri", "logo_uri", "created_at", "updated_at", "deleted_at", "client_type") FROM stdin;
+COPY "auth"."oauth_clients" ("id", "client_secret_hash", "registration_type", "redirect_uris", "grant_types", "client_name", "client_uri", "logo_uri", "created_at", "updated_at", "deleted_at", "client_type", "token_endpoint_auth_method") FROM stdin;
 \.
 
 
@@ -1203,6 +1203,6 @@ SELECT pg_catalog.setval('"public"."migrations_id_seq"', 99, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict YffVj3hQEOXHOhWPIKdPcSZy36CtsrZbUR8l1QXGLxuSw4lidL6ulYD5Cbcfpcb
+-- \unrestrict kDbqjkDGskJ9Safm2mzYiOA98fRzQNI4qQq60WfRLRJBe8QJapor1raRJCgDEIP
 
 RESET ALL;
