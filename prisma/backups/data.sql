57c7a45a210578e62f0518a975b7b3c5944df335
xycasupanan <xycasupanan@users.noreply.github.com>
2026-03-21 02:36:46 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict txDmKOC2MLX9hHbxvqOLQKwTWutNni1b7KGXVixby4J0cAYfRcDcOF1cN4vwIXI
+-- \restrict G25qQAr6ehuclk503Mb2oidElDGvwEk9tMIwrVZlsCLkBZs0vkBGCtMe1udlmd9
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -178,6 +178,22 @@ COPY "auth"."saml_relay_states" ("id", "sso_provider_id", "request_id", "for_ema
 --
 
 COPY "auth"."sso_domains" ("id", "sso_provider_id", "domain", "created_at", "updated_at") FROM stdin;
+\.
+
+
+--
+-- Data for Name: webauthn_challenges; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."webauthn_challenges" ("id", "user_id", "challenge_type", "session_data", "created_at", "expires_at") FROM stdin;
+\.
+
+
+--
+-- Data for Name: webauthn_credentials; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."webauthn_credentials" ("id", "user_id", "credential_id", "public_key", "attestation_type", "aaguid", "sign_count", "transports", "backup_eligible", "backed_up", "friendly_name", "created_at", "updated_at", "last_used_at") FROM stdin;
 \.
 
 
@@ -1211,6 +1227,6 @@ SELECT pg_catalog.setval('"public"."migrations_id_seq"', 99, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict txDmKOC2MLX9hHbxvqOLQKwTWutNni1b7KGXVixby4J0cAYfRcDcOF1cN4vwIXI
+-- \unrestrict G25qQAr6ehuclk503Mb2oidElDGvwEk9tMIwrVZlsCLkBZs0vkBGCtMe1udlmd9
 
 RESET ALL;
